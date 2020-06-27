42d3d7979c09a6b39aa2581c2a6c35563bac14e2
yeweirui <yeweirui16@gmail.com>
2020-06-27 23:09:13 +0800

    change relu to tanh

--- stable_baselines/common/policies.py
+++ stable_baselines/common/policies.py
@@ -20,7 +20,8 @@ def cnn_typeVector(concat_vector, **kwargs):
     batch = tf.shape(scaled_images)[0]
     scaled_images = tf.reshape(scaled_images, [batch, 64, 64, 4])
 
-    activ = tf.nn.relu
+    # activ = tf.nn.relu
+    activ = tf.nn.tanh
     layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=8, stride=4, init_scale=np.sqrt(2), **kwargs))
     layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
     layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
@@ -39,7 +40,8 @@ def nature_cnn(scaled_images, **kwargs):
     :param kwargs: (dict) Extra keywords parameters for the convolutional layers of the CNN
     :return: (TensorFlow Tensor) The CNN output layer
     """
-    activ = tf.nn.relu
+    # activ = tf.nn.relu
+    activ = tf.nn.tanh
     layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=8, stride=4, init_scale=np.sqrt(2), **kwargs))
     layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
     layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
@@ -583,7 +585,7 @@ class FeedForwardPolicy(ActorCriticPolicy):
             self._value_fn = linear(vf_latent, 'vf', 1)
 
             self._proba_distribution, self._policy, self.q_value = \
-                self.pdtype.proba_distribution_from_latent(pi_latent, vf_latent, init_scale=0.01)
+                self.pdtype.proba_distribution_from_latent(pi_latent, vf_latent, init_scale=0.005)
 
         self._setup_init()
 
